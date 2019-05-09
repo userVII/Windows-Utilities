4653158dcaff3331a1b52d5ba7ea622e4a92d9ef
M W <userVII@users.noreply.github.com>
2019-05-09 11:43:46 -0400

    Create Custom Event Trigger.ps1
    Used to further filter a Scheduled Task event filter. This is to support wildcards

---
+++ Custom Event Trigger.ps1
@@ -0,0 +1,27 @@
+#Get very recent USB trigger
+$Query = @"
+    <QueryList>
+        <Query Id="0" Path="Microsoft-Windows-DriverFrameworks-UserMode/Operational">
+            <Select Path="Microsoft-Windows-DriverFrameworks-UserMode/Operational">
+                *[System[(EventID=2003) and TimeCreated[timediff(@SystemTime) &lt;= 300000000]]]
+            </Select>
+        </Query>
+    </QueryList>
+"@
+
+$productID = "*SOMEID*"
+$Events = Get-WinEvent -FilterXml $Query
+$eventHolder = $Events | Select Message
+$eventObjectHolder = @()
+
+foreach($event in $eventHolder){
+    if($event.Message -like "$productID"){
+        $eventObject = New-Object System.Object
+        $eventObject | Add-Member -MemberType NoteProperty -Name "InstanceID" -Value $event.Message
+        $eventObjectHolder += $eventObject
+    }    
+}
+
+if($eventObjectHolder.Count -gt 0){
+    $eventObjectHolder | Out-GridView -PassThru -Title "Found $($eventObjectHolder.Count) events"
+}
