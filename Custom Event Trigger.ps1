8d0df0c25e78848967db3fd12946775d9917af3e
M W <userVII@users.noreply.github.com>
2019-05-09 11:44:29 -0400

    Update Custom Event Trigger.ps1

--- Custom Event Trigger.ps1
+++ Custom Event Trigger.ps1
@@ -3,7 +3,7 @@ $Query = @"
     <QueryList>
         <Query Id="0" Path="Microsoft-Windows-DriverFrameworks-UserMode/Operational">
             <Select Path="Microsoft-Windows-DriverFrameworks-UserMode/Operational">
-                *[System[(EventID=2003) and TimeCreated[timediff(@SystemTime) &lt;= 300000000]]]
+                *[System[(EventID=2003) and TimeCreated[timediff(@SystemTime) &lt;= 3000]]]
             </Select>
         </Query>
     </QueryList>
