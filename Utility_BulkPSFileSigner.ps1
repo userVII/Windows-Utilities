6f9ef4dced799632f1fb6a66ad5f4c228fabf7fc
M W <userVII@users.noreply.github.com>
2019-06-07 13:08:49 -0400

    Update Utility_BulkPSFileSigner.ps1

--- Utility_BulkPSFileSigner.ps1
+++ Utility_BulkPSFileSigner.ps1
@@ -1,17 +1,33 @@
-$filesRoot = ""
-$filesToSignPaths = Get-ChildItem $filesRoot -Recurse -Include *.ps1, *.psm1
-
 $certFriendlyName = ""
 $codeSigningCertificateThumbprint = ""
 $cert = Get-ChildItem Cert:\CurrentUser\My\$codeSigningCertificateThumbprint -CodeSigningCert 
 $timeStampURL = "http://timestamp.comodoca.com/authenticode"
 
-foreach($file in $filesToSignPaths){
-    if($cert) {
-        Set-AuthenticodeSignature -filepath "$filesRoot\$file" -cert $cert -IncludeChain All -TimeStampServer $timeStampURL
+function Sign_PowerShellFiles($pathToSign){
+    $counter = 1
+    $filestosignpath = Get-ChildItem $pathToSign -Recurse -Include *.ps1, *.psm1
+    $filesCount = ($filestosignpath | Measure-Object).Count
+
+    foreach($file in $filestosignpath){
+        Write-Progress -Activity "Working on $counter of $filesCount" -status "Signing $file"  -PercentComplete (($counter / $filesCount)*100)
+
+        if($cert) {
+            Set-AuthenticodeSignature -filepath $file -cert $cert -IncludeChain All -TimeStampServer $timeStampURL
+        }
+        else {
+            throw "Did not find certificate with friendly name of `"$certFriendlyName`""
+        }
+        if($filesCount -gt 1){
+            Start-Sleep -Seconds 15 #To not blow up their time server
+        }
     }
-    else {
-        throw "Did not find certificate with friendly name of `"$certFriendlyName`""
-    }
-    Start-Sleep -Seconds 15 #To not blow up their time server
 }
+
+[string]$powershellFileLoc = Read-Host -Prompt 'Root of the PowerShell files?'
+$powershellFileLoc = $powershellFileLoc.Replace('"',"")
+
+if(Test-Path $powershellFileLoc){
+    Sign_PowerShellFiles $powershellFileLoc
+}
+
+$pauser = Read-Host -Prompt 'Press any key to continue...'
