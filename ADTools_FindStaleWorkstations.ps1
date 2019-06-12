86cadf4a49aabb569c3de20079c3b9125a6ba66e
M W <userVII@users.noreply.github.com>
2019-06-12 11:36:58 -0400

    Update ADTools_FindStaleWorkstations.ps1

--- ADTools_FindStaleWorkstations.ps1
+++ ADTools_FindStaleWorkstations.ps1
@@ -1,5 +1,6 @@
 $DaysInactive = 180
 $OUtosearch = ""
+$PCNamesToIgnore = @("*SCCM*")
 
 $time = (Get-Date).Adddays(-($DaysInactive))
 $pclist = Get-ADComputer -SearchBase $OUtosearch -Filter {LastLogonTimeStamp -lt $time} -ResultPageSize 2000 -resultSetSize $null -Properties Name, OperatingSystem, SamAccountName, DistinguishedName
@@ -8,8 +9,14 @@ $counter = 1
 
 foreach ($pc in $pclist){
     Write-Progress -Activity "Updating PC description $counter / $pccount" -status "Working on $($pc.Name)"  -percentComplete (($counter / $pccount)*100)
-    Write-Host "$($pc.Name) is being updated"
-    Set-ADComputer $pc.Name -Description "***This computer has been inactive for $DaysInactive or more days***"
+    $pcName = $pc.Name
+    foreach($ignoreName in $PCNamesToIgnore){
+        if($pcName -like $ignoreName){
+            Write-Host "$pcName is on the ignore list" -ForegroundColor Yellow
+        }else{            
+            Write-Host "$($pc.Name) is being updated"
+            Set-ADComputer $pc.Name -Description "***This computer has been inactive for $DaysInactive or more days***"
+        }
+    }
     $counter++
 }
-
